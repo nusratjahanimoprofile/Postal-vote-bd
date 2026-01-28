8b22d5c8a480d9a35b821daae8e5c9e2a4651211
Nurat jahan <shimakhatun00990@gmail.com>
2026-01-29 00:51:21 +0600

    Create HelpCenter.tsx

---
+++ src/components/HelpCenter.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Language } from '../types';
+const HelpCenter: React.FC<{ lang: Language }> = () => (
+  <div className="p-6 bg-white rounded-2xl shadow-xl">Help Center</div>
+);
+export default HelpCenter;
